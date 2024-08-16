af43f107ed1a5db363462a76ccb0d38856b75f3f
UnmoveD <unmoved2121@gmail.com>
2024-08-16 22:44:24 +0200

    garnet: AlphaDroid Bring-Up

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -14,18 +14,37 @@ $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
 
-# Derp Stuff
-TARGET_HAS_UDFPS := true
-EXTRA_UDFPS_ANIMATIONS := true
+# Maintainer
+ALPHA_MAINTAINER := UnmoveD
+ALPHA_BUILD_TYPE := Official
+
+# Device config
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BUILD_PACKAGE := 3
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# TARGET_LAUNCHER options:
+# 1 - stock (default)
+# 2 - lawnchair
+# 3 - pixel (valid only on gapps builds)
+TARGET_LAUNCHER := 1
+
+# GAPPS (valid only for GAPPS builds)
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_MATLOG := true
 
-# Derpfest
-DERP_BUILDTYPE := Official
-
-#GApps
-TARGET_USES_PICO_GAPPS := true
-
-PRODUCT_NAME := derp_garnet
+PRODUCT_NAME := lineage_garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
